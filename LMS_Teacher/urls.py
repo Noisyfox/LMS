5655fa59166b942cf9962b18e13658b004c8ef35
noisyfox <timemanager.rick@gmail.com>
2016-10-19 05:29:41 +1100

    Add teacher unit info page

--- LMS_Teacher/urls.py
+++ LMS_Teacher/urls.py
@@ -6,4 +6,7 @@ app_name = 'lms_tec'
 
 urlpatterns = [
     url(r'^my_units/$', views.UnitListView.as_view(), name='unit'),
+    url(r'^unit/(?P<unit_id>[0-9]+)/', include([
+        url(r'^info/$', views.UnitInfoView.as_view(), name='unit_info'),
+    ]))
 ]
--- LMS_Teacher/views.py
+++ LMS_Teacher/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 from django.views.generic import ListView
 
+from LMS.mixins import QueryMixin
 from LMS.models import Unit
 from LMS_Teacher.mixins import TeacherMixin
+
+
+class UnitQueryMixin(QueryMixin):
+    def do_query(self, request, *args, **kwargs):
+        unit = get_object_or_404(Unit, Q(staff=self.request.user.teacher) & Q(pk=kwargs['unit_id']))
+
+        self._unit = unit
+
+    @property
+    def unit(self):
+        if not self._unit:
+            raise Http404('Unknown unit.')
+
+        return self._unit
+
+    def get_context_data(self, **kwargs):
+        ctx = super(UnitQueryMixin, self).get_context_data(**kwargs)
+
+        ctx['unit'] = self._unit
+
+        return ctx
 
 
 class UnitListView(TeacherMixin, ListView):
@@ -12,3 +37,10 @@ class UnitListView(TeacherMixin, ListView):
 
     def get_queryset(self):
         return Unit.objects.filter(staff=self.request.user.teacher)
+
+
+class UnitInfoView(TeacherMixin, UnitQueryMixin, DetailView):
+    template_name = 'LMS_Teacher/unit_info.html'
+
+    def get_object(self, queryset=None):
+        return self.unit
