d9e386ab0dfd1421b87017c2d3f4ea6dd2cccacb
noisyfox <timemanager.rick@gmail.com>
2016-10-19 20:05:17 +1100

    Unit info pages

--- LMS_Student/urls.py
+++ LMS_Student/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^my_units/$', views.UnitListView.as_view(), name='unit'),
     url(r'^enroll/$', views.EnrollListView.as_view(), name='enroll'),
     url(r'^unit/(?P<unit_id>[0-9]+)/', include([
+        url(r'^info/$', views.UnitInfoView.as_view(), name='unit_info'),
     ])),
 ]
--- LMS_Student/views.py
+++ LMS_Student/views.py
@@ -5,20 +5,12 @@ from django.http import Http404
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
+from django.views.generic import DetailView
 from django.views.generic import ListView
 
 from LMS.mixins import QueryMixin
 from LMS.models import Unit
 from LMS_Student.mixins import StudentMixin
-
-
-class UnitListView(StudentMixin, ListView):
-    template_name = 'LMS_Student/unit.html'
-    context_object_name = 'units'
-    allow_empty = True
-
-    def get_queryset(self):
-        return self.request.user.student.enrolled_unit.all()
 
 
 class UnitQueryMixin(QueryMixin):
@@ -42,6 +34,15 @@ class UnitQueryMixin(QueryMixin):
         return ctx
 
 
+class UnitListView(StudentMixin, ListView):
+    template_name = 'LMS_Student/unit.html'
+    context_object_name = 'units'
+    allow_empty = True
+
+    def get_queryset(self):
+        return self.request.user.student.enrolled_unit.all()
+
+
 class EnrollListView(StudentMixin, ListView):
     template_name = 'LMS_Student/enroll.html'
     context_object_name = 'units'
@@ -54,3 +55,10 @@ class EnrollListView(StudentMixin, ListView):
         self.request.user.student.enrolled_unit = units
         self.request.user.student.save()
         return HttpResponseRedirect(reverse_lazy('lms_stu:unit'))
+
+
+class UnitInfoView(StudentMixin, UnitQueryMixin, DetailView):
+    template_name = 'LMS_Student/unit_info.html'
+
+    def get_object(self, queryset=None):
+        return self.unit
